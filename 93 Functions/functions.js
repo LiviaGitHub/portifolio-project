9c3f5eb79f1954d1b9e008d96ee231c325695c04
Livia Bonifacio <50983740+LiviaGitHub@users.noreply.github.com>
2024-08-09 09:29:56 +0200

    changes

---
+++ 93 Functions/functions.js
@@ -0,0 +1,32 @@
+function split(cash, cost) {
+    var bottles = Math.floor(cash / cost);
+    return bottles;
+}
+
+function getMilk(cash, cost) {   
+    var bottles = split(cash, cost);
+
+    console.log("leaveHouse");
+    console.log("moveRight");
+    console.log("moveRight");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveRight");
+    console.log("moveRight");
+
+    console.log("buy " + bottles + " bottle(s) of milk");
+
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("enterHouse");
+}
+
+getMilk(10, 2);
